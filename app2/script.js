cb7db05cfa277dc557ad453000853cbf49f83ea6
Satyam Dixit <satyamdixit2003@gmail.com>
2025-08-03 04:26:47 +0530

    work done

---
+++ app2/script.js
@@ -0,0 +1,13 @@
+
+const heading = document.querySelector('h1') ;
+
+
+const allListItems = document.querySelectorAll('ul li') ;
+
+for (let i = 0; i < allListItems.length; i++) {
+    const element = allListItems[i];
+    element.innerText = "hello" ;
+    
+}
+console.log(allListItems);
+let ind = [1,5,77,4,69,18];
